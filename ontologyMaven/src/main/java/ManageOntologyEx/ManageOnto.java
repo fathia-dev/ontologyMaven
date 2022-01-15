accdbd48e6f6a0bc434c0bd9e796189cde7b8b94
asus <asus@192.168.1.11>
2022-01-15 19:02:38 +0100

    first commit

---
+++ ontologyMaven/src/main/java/ManageOntologyEx/ManageOnto.java
@@ -0,0 +1,14 @@
+package ManageOntologyEx;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public class ManageOnto {
+	private OWLOntology ontology;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
